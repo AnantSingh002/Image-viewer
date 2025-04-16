7e7939b70ef67d56a78b39b205f77473e3667d66
Anant <anantsinghchouhan@gmail.com>
2025-04-16 23:42:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container'>
+
+      
+      <div className='box' >
+      <img src="/vite.svg" alt="" srcset="" />
+      </div>
+      <div className='box'>
+      <img src="/vite.svg" alt="" srcset="" />
+      </div>
+      <div className='box'>
+      <img src="/vite.svg" alt="" srcset="" />
+      </div>
+      <div className='box'>
+      <img src="/vite.svg" alt="" srcset="" />
+      </div>
+      
+      </div>
+
+
+     <div>
+
+     </div>
+    </>
+  )
+}
+
+export default App
